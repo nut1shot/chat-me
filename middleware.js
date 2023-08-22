88bdf092ac0f79fd750e0e47c93a0c5133d9166d
nut1shot <nutp10.1@gmail.com>
2023-08-22 12:56:06 +0700

    update demo url

--- middleware.js
+++ middleware.js
@@ -4,10 +4,10 @@ export default function middleware(req, res, next) {
   let verify = req.cookies.get("user");
   let url = req.url;
   if (!verify && url.includes("/chat")) {
-    return NextResponse.redirect("http://localhost:3000/");
+    return NextResponse.redirect("https://nut-chat-me.vercel.app/");
   }
 
-  if (verify && url === "http://localhost:3000/") {
-    return NextResponse.redirect("http://localhost:3000/");
+  if (verify && url === "https://nut-chat-me.vercel.app/") {
+    return NextResponse.redirect("https://nut-chat-me.vercel.app/");
   }
 }
